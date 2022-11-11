b0bf79fd6d74188db023dc1574a61475fc748e7d
Shawn Lapuz <shangel.lapuz@gmail.com>
2022-11-11 21:38:35 +0800

    Loader and Error Component

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -10,6 +10,11 @@ const Discover = () => {
 
 	const genreTitle = 'Pop'
 	// console.log(genres);
+	
+	if(isFetching) return <Loader title="Loading songs..."/>
+
+	if (error) return <Error />
+
 	return (
     <div className="flex flex-col">
 			<div className="w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10">
@@ -26,7 +31,7 @@ const Discover = () => {
 			</div>
 
 			<div className="flex flex-wrap sm:justify-start justify-center gap-8">
-				{[1,2,3,4,5,6,7,8,9].map((song, i) => (
+				{data?.map((song, i) => (
 					<SongCard
 						key={song.key}
 						song={song}
