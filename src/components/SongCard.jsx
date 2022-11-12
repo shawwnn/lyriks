a6f8cfdea343d66950f082c43530f80fbfd79eb9
Shawn Lapuz <shangel.lapuz@gmail.com>
2022-11-12 14:35:56 +0800

    Song Card IV: show play or pause icon

--- src/components/SongCard.jsx
+++ src/components/SongCard.jsx
@@ -17,7 +17,9 @@ const SongCard = ({ song, isPlaying, activeSong, i, data }) => {
     <div className="flex flex-col w-[250px] p-4 bg-white/5 bg-opacity-80 backdrop-blur-sm animate-slideup rounded-lg cursor-pointer">
       <div className="relative w-full h-56 group">
         <div className={`rounded-md absolute inset-0 justify-center items-center bg-black bg-opacity-50 group-hover:flex ${activeSong?.title === song.title ? 'flex bg-black bg-opacity-70' : 'hidden'} `}>
-          <PlayPause 
+          <PlayPause
+            isPlaying={isPlaying}
+            activeSong={activeSong} 
             song={song}
             handlePause={handlePauseClick}
             handlePlay={handlePlayClick}
