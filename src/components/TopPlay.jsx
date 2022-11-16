909673508e85235ccf5b24588422c2aa04e237d6
Shawn Lapuz <shangel.lapuz@gmail.com>
2022-11-16 14:35:25 +0800

    Top Plays I: declaration of variables needed

--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -1,5 +1,33 @@
-const TopPlay = () => (
-  <div>TopPlay</div>
-);
+import { useEffect,useRef } from "react";
+import { Link } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { FreeMode } from "swiper";
+
+import PlayPause from "./PlayPause";
+import { playPause, setActiveSong } from "../redux/features/playerSlice";
+import { useGetTopChartsQuery } from "../redux/services/shazamCore";
+
+import 'swiper/css';
+import 'swiper/css/free-mode';
+
+const TopPlay = () => {
+  const dispatch = useDispatch();
+  const { activeSong, isPlaying } = useSelector((state) => state.player);
+  const { data } = useGetTopChartsQuery();
+  const divRef = useRef(null);
+
+  const topPlays = data?.slice(0, );
+
+  const handlePauseClick = () => {
+    dispatch(playPause(false));
+  };
+
+  const handlePlayClick = () => {
+    dispatch(setActiveSong({ song, data, i}));
+    dispatch(playPause(true));
+  };
+
+};
 
 export default TopPlay;
