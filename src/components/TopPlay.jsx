d5354636db3a76005ecb8c1c6f5a2a0ab7a23439
Shawn Lapuz <shangel.lapuz@gmail.com>
2022-12-08 07:23:12 +0800

    Top Artist Chart - UI and Links

--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -14,7 +14,20 @@ import { TopCharts } from "../pages";
 
 const TopChartCard = ({ song, i }) => (
   <div className="w-full flex flex-row items-center hover:bg-[#4C426E] py-2 p-4 rounded-lg cursor-pointer mb-2">
-    {song.title}
+    {/* {song.title} */}
+    <h3 className="font-bold text-base text-white mr-3">{i + 1}.</h3>
+    <div className="flex-1 flex flex-row justify-between items-center">
+      <img src={song?.images?.coverart} alt={song?.title} 
+      className="w-20 h-20 rounded-lg"/>
+      <div className="flex-1 flex flex-col justify-center mx-3">
+        <Link to={`/song/${song.key}`}>
+          <p className="text-xl font-bold text-white">{song?.title}</p>
+        </Link>
+        <Link to={`/artists/${song?.artists[0].adamid}`}>
+          <p className="text-base font-bold text-gray-300">{song?.subtitle}</p>
+        </Link>
+      </div>
+    </div>
   </div>
 );
 
