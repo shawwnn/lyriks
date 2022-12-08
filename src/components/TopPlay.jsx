a5f12a1ac0bd496ab95cb406698943f7dbec1d85
Shawn Lapuz <shangel.lapuz@gmail.com>
2022-12-08 09:11:10 +0800

    Fetch Params so we can use for more song details

--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -20,7 +20,7 @@ const TopChartCard = ({ song, i, isPlaying, activeSong, handlePauseClick, handle
       <img src={song?.images?.coverart} alt={song?.title} 
       className="w-20 h-20 rounded-lg"/>
       <div className="flex-1 flex flex-col justify-center mx-3">
-        <Link to={`/song/${song.key}`}>
+        <Link to={`/songs/${song.key}`}>
           <p className="text-xl font-bold text-white">{song?.title}</p>
         </Link>
         <Link to={`/artists/${song?.artists[0].adamid}`}>
--- src/pages/SongDetails.jsx
+++ src/pages/SongDetails.jsx
@@ -1,3 +1,12 @@
-const SongDetails = () => <div>SongDetails</div>;
+import { useParams } from "react-router-dom";
+
+const SongDetails = () => {
+	const { songid} = useParams();
+	console.log(songid);
+
+  return (
+		<div>SongDetails</div>
+	)
+};
 
 export default SongDetails;
