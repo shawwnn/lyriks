41cde8b1f71fb07722bdf2b1756a0d1e7ab83e34
Shawn Lapuz <shangel.lapuz@gmail.com>
2022-12-08 09:13:32 +0800

    loaded imports needed for song artists page

--- src/pages/SongDetails.jsx
+++ src/pages/SongDetails.jsx
@@ -1,4 +1,8 @@
 import { useParams } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { DetailsHeader, Error, Loader, RelatedSongs } from "../components";
+import { setActiveSong, playPause } from "../redux/features/playerSlice";
+
 
 const SongDetails = () => {
 	const { songid} = useParams();
