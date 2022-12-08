9d316c66bfb06490201296b6600e553e66c0acf3
Shawn Lapuz <shangel.lapuz@gmail.com>
2022-12-08 23:32:44 +0800

    apply Link to redirect to artist when Artist is clicked

--- src/components/DetailsHeader.jsx
+++ src/components/DetailsHeader.jsx
@@ -19,12 +19,20 @@ const DetailsHeader = ({ artistId, artistData, songData }) => {
         <div className="ml-5">
           <p className="font-bold sm:text-3xl text-xl text-white">{artistId ? artist.name : songData?.title}</p>
           {!artistId && (
-            <Link>
+            <Link to={`/artists/${songData?.artists[0].adamid}`}>
               <p className="text-base text-gray-400 mt-2">{songData?.subtitle}</p>
             </Link>
           )}
+
+          <p className="text-base text-gray-400 mt-2">
+            {artistId
+              ? artist?.genreNames[0]
+              : songData?.genres?.primary}
+          </p>
         </div>
       </div>
+
+      <div className="w-full sm:h-44 h-24" />
     </div>
   )
 };
