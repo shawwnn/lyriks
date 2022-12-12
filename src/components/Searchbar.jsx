278ffd51620a440ec473fb7edc1a66559ab3cbc6
Shawn Lapuz <shangel.lapuz@gmail.com>
2022-12-12 15:39:15 +0800

    Search I: made handle submit for form where usenavigate is used
    change the value and onChange attrib of input element

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -4,23 +4,35 @@ import { useNavigate } from "react-router-dom";
 
 import { FiSearch } from "react-icons/fi";
 
-const Searchbar = () => (
-  <form autoComplete="off" className="p-2 text-gray-400 focus-within:text-gray-600">
-    <label htmlFor="search-field" className="sr-only">Search All Songs</label>
+const Searchbar = () => {
+  const navigate = useNavigate();
+  const [searchTerm, setSearchTerm] = useState('')
+  
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-    <div className="flex flex-row items-center justify-start">
-      <FiSearch className="w-5 h-5 ml-4"/>
-      <input type="text" 
-        name="search-field" 
-        autoComplete="off"
-        id="search-field"
-        placeholder="Search"
-        value=""
-        onChange={() => {}}
-        className="flex-1 bg-transparent border-none outline-none placeholder-gray-500 text-base text-white p-4"
-      />
-    </div>
-  </form>
-);
+    navigate(`/search/${searchTerm}`);
+
+  }
+
+  return (
+    <form onSubmit={handleSubmit} autoComplete="off" className="p-2 text-gray-400 focus-within:text-gray-600">
+      <label htmlFor="search-field" className="sr-only">Search All Songs</label>
+
+      <div className="flex flex-row items-center justify-start">
+        <FiSearch className="w-5 h-5 ml-4"/>
+        <input type="text" 
+          name="search-field" 
+          autoComplete="off"
+          id="search-field"
+          placeholder="Search"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          className="flex-1 bg-transparent border-none outline-none placeholder-gray-500 text-base text-white p-4"
+        />
+      </div>
+    </form>
+  )
+};
 
 export default Searchbar;
